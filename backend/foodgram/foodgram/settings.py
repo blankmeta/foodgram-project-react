ede1368461cc008a44caedc95b2e40b48b6865ff
blankmeta <yadanilfilatov@gmail.com>
2023-03-09 14:48:22 +0300

    Tags fix

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -16,6 +16,7 @@ DEBUG = os.getenv('DEBUG', default=True)
 
 LOCAL = False
 
+
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['*'])
 CSRF_TRUSTED_ORIGINS = ['https://*.84.201.178.138', 'https://*.127.0.0.1',
                         'http://*.84.201.178.138', 'http://*.127.0.0.1']
