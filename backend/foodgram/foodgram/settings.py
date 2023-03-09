49f81b4d93b99b145fdf9f606b3715e79c31ce82
blankmeta <yadanilfilatov@gmail.com>
2023-03-09 13:17:33 +0300

    CSRF fixes

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -18,6 +18,8 @@ LOCAL = False
 
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '84.201.178.138']
+CSRF_TRUSTED_ORIGINS = ['https://*.84.201.178.138', 'https://*.127.0.0.1',
+                        'http://*.84.201.178.138', 'http://*.127.0.0.1',]
 
 # Application definition
 
