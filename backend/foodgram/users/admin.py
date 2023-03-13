db68ff0cc37fb70633b508e564bf89335e2db2c2
blankmeta <yadanilfilatov@gmail.com>
2023-03-13 16:00:37 +0300

    More info in admin panel

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
+from users.models import Subscription
+
 User = get_user_model()
 
 admin.site.unregister(User)
@@ -11,3 +13,8 @@ admin.site.unregister(User)
 class UserAdmin(UserAdmin):
     list_filter = (
         'username', 'email', 'is_staff', 'is_superuser', 'is_active', 'groups')
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
